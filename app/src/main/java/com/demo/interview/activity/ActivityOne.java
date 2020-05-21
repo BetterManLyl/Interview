7c07e360b76d8421f56e67ffa2eaecd3e5f3e759
ylli10 <ylli10@iflytek.com>
2020-05-21 09:24:56 +0800

    三方依赖

---
+++ app/src/main/java/com/demo/interview/activity/ActivityOne.java
@@ -0,0 +1,23 @@
+package com.demo.interview.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 文 件 名：ActivityOne
+ * 创 建 人：李跃龙
+ * 创建日期：2020/5/21 9:17
+ * 邮    箱：ylli10@iflytek.com
+ * 功    能：
+ * 修 改 人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class ActivityOne extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/demo/interview/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.demo.interview.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 文 件 名：BaseActivity
+ * 创 建 人：李跃龙
+ * 创建日期：2020/5/21 9:18
+ * 邮    箱：ylli10@iflytek.com
+ * 功    能：
+ * 修 改 人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class BaseActivity extends AppCompatActivity {
+}
