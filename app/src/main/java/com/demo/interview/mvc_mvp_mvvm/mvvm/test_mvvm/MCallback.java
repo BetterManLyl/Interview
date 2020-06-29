79a0187cdf812913994de07416398876d88b2ecc
ylli10 <ylli10@iflytek.com>
2020-06-29 18:26:10 +0800

    测试 本地提交

---
+++ app/src/main/java/com/demo/interview/mvc_mvp_mvvm/mvvm/test_mvvm/MCallback.java
@@ -0,0 +1,16 @@
+package com.demo.interview.mvc_mvp_mvvm.mvvm.test_mvvm;
+
+/**
+ * 文 件 名：MCallback
+ * 创 建 人：李跃龙
+ * 创建日期：2020/6/15 23:41
+ * 邮    箱：ylli10@iflytek.com
+ * 功    能：
+ * 修 改 人：
+ * 修改时间：
+ * 修改备注：
+ */
+public interface MCallback {
+    public void onSuccess(Account account);
+     public  void onFailed();
+}
